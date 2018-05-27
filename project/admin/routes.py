9dc50fa52716eba0942b0534c75f90d47b689d3f
Mamun Hasan <mamuncse2k5@gmail.com>
2018-05-28 02:44:38 +0600

    blueprint app

--- project/admin/routes.py
+++ project/admin/routes.py
@@ -1,9 +1,8 @@
-from flask import Blueprint, render_template, abort
-from jinja2 import TemplateNotFound
+from flask import Blueprint, render_template
 
 admin_index = Blueprint('admin_index', __name__, template_folder='templates')
 
 
 @admin_index.route('/')
-def hello():
-    return 'Hello, World!'
+def dashboard():
+    return render_template('dashboard.html', title="Dashboard")
--- project/site/routes.py
+++ project/site/routes.py
@@ -5,5 +5,5 @@ site_index = Blueprint('site_index', __name__, template_folder='templates')
 
 
 @site_index.route('/')
-def hello():
-    return 'Hello, World!'
+def home():
+    return render_template('home.html', title="Home")
