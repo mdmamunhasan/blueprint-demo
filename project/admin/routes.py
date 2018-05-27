cf5791a48a0d3a4346b3a0198bd3c6c8fb06f076
Mamun Hasan <mamuncse2k5@gmail.com>
2018-05-28 04:05:10 +0600

    blueprint app

--- project/admin/routes.py
+++ project/admin/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 
 admin_index = Blueprint('admin_index', __name__, template_folder='templates')
 
@@ -6,3 +6,8 @@ admin_index = Blueprint('admin_index', __name__, template_folder='templates')
 @admin_index.route('/')
 def dashboard():
     return render_template('dashboard.html', title="Dashboard")
+
+
+@admin_index.route('/login')
+def login():
+    return redirect(url_for('admin_index.dashboard'))
--- project/site/routes.py
+++ project/site/routes.py
@@ -6,3 +6,7 @@ site_index = Blueprint('site_index', __name__, template_folder='templates')
 @site_index.route('/')
 def home():
     return render_template('home.html', title="Home")
+
+@site_index.route('/register')
+def register():
+    return render_template('register.html', title="Register")
