86db1b1eeca63073b0ac18353db103bd78e4bc91
Mamun Hasan <mamunhasan@w3engineers.com>
2018-05-28 12:25:38 +0600

    wtf forms

--- project/api/routes.py
+++ project/api/routes.py
@@ -3,7 +3,7 @@ from flask import Blueprint, jsonify
 api_index = Blueprint('api_index', __name__)
 
 
-@api_index.route('/')
+@api_index.route('/hello')
 def hello():
     d = {
         "title": "Hello, World!"
