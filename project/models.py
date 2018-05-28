19975b9a43f55d16a10d4deb380ca10c21c9ecce
Mamun Hasan <mamuncse2k5@gmail.com>
2018-05-29 01:26:52 +0600

    retrieve data dynamodb

--- project/models.py
+++ project/models.py
@@ -1,4 +1,5 @@
-import boto3
+import boto3, json, decimal
+from botocore.exceptions import ClientError
 
 # For a Boto3 client.
 ddb = boto3.resource(
@@ -8,6 +9,17 @@ ddb = boto3.resource(
     aws_access_key_id='anything',
     aws_secret_access_key='anything'
 )
+
+
+# Helper class to convert a DynamoDB item to JSON.
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
 
 
 def list_tables():
@@ -58,4 +70,25 @@ def create_movie():
     )
 
 
-create_movie()
+def get_movie():
+    table = ddb.Table('Movies')
+
+    title = "Abstraction"
+    year = 2012
+
+    try:
+        response = table.get_item(
+            Key={
+                'year': year,
+                'title': title
+            }
+        )
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        item = response['ResponseMetadata']
+        print("GetItem succeeded:")
+        print(json.dumps(item, indent=4, cls=DecimalEncoder))
+
+
+get_movie()
