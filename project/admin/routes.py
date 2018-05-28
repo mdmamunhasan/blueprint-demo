cd5c2dec9e07e7e18abd5546e6cd4fb32ed4e356
Mamun Hasan <mamunhasan@w3engineers.com>
2018-05-28 10:17:38 +0600

    dynamodb connection

--- project/admin/routes.py
+++ project/admin/routes.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, flash
 
 admin_index = Blueprint('admin_index', __name__, template_folder='templates')
+
+from project.admin.forms import LoginForm
 
 
 @admin_index.route('/')
@@ -8,6 +10,11 @@ def dashboard():
     return render_template('dashboard.html', title="Dashboard")
 
 
-@admin_index.route('/login')
+@admin_index.route('/login', methods=['GET', 'POST'])
 def login():
-    return redirect(url_for('admin_index.dashboard'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        # Todo database and session management
+        flash('Thanks for registering')
+        return redirect(url_for('admin_index.dashboard'))
+    return render_template('login.html', title="Login", form=form)
---
+++ project/models.py
@@ -0,0 +1,13 @@
+import boto3
+
+# For a Boto3 client.
+ddb = boto3.client(
+    'dynamodb',
+    endpoint_url='http://localhost:8000',
+    region_name='ap-southeast-1',
+    aws_access_key_id='anything',
+    aws_secret_access_key='anything'
+)
+response = ddb.list_tables()
+print(response)
+
