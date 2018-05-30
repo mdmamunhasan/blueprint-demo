bb6f67e44687f6e6f4a28e620b7bffd2fd462aaa
Mamun Hasan <mamuncse2k5@gmail.com>
2018-05-31 01:50:43 +0600

    blueprint session

--- project/admin/routes.py
+++ project/admin/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, session, redirect, url_for, flash
+from flask import Blueprint, render_template, request, session, redirect, url_for, flash, g
 from project.admin.models import check_user
 
 admin_index = Blueprint('admin_index', __name__, template_folder='templates')
@@ -6,11 +6,18 @@ admin_index = Blueprint('admin_index', __name__, template_folder='templates')
 from project.admin.forms import LoginForm
 
 
+@admin_index.before_request
+def before_request():
+    g.user = None
+    if 'user' in session:
+        g.user = session['user']
+
+
 @admin_index.route('/')
 def dashboard():
-    if 'email' not in session:
-        return redirect(url_for('admin_index.login'))
-    return render_template('dashboard.html', title="Dashboard")
+    if g.user:
+        return render_template('dashboard.html', title="Dashboard")
+    return redirect(url_for('admin_index.login'))
 
 
 @admin_index.route('/login', methods=['GET', 'POST'])
@@ -24,10 +31,10 @@ def login():
         flash('Thanks for registering')
         if check_user(email, password):
             flash('Thanks for registering')
-            session['email'] = request.form['email']
+            session['user'] = request.form['email']
             return redirect(url_for('admin_index.dashboard'))
 
-    if 'email' in session:
+    if g.user:
         return redirect(url_for('admin_index.dashboard'))
     else:
         return render_template('login.html', title="Login", form=form)
@@ -35,5 +42,5 @@ def login():
 
 @admin_index.route('/logout', methods=['GET'])
 def logout():
-    session.pop('email', None)
+    session.pop('user', None)
     return redirect(url_for('admin_index.login'))
